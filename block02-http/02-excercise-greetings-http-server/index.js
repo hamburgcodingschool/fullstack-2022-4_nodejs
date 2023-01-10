1ce1cd3057c5918121a6b1f79a19c2c147aef995
Tim Adler <mail@tim-adler.de>
2023-01-10 21:36:14 +0100

    added exercise

---
+++ block02-http/02-excercise-greetings-http-server/index.js
@@ -0,0 +1,6 @@
+http = require('http');
+_ = require('lodash');
+
+const greetings = ["Hello",  "Bonjour",  "Hola",  "Ciao",  "Hallo",  "こんにちは",  "안녕하세요",  "你好",  "Olá",  "Привет"]
+
+// Create a server that greets the user in a random language
