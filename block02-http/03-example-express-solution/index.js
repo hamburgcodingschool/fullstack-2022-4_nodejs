01cf95f2d5f403d9dd1d02b5021c0ca317f4a5ab
Tim Adler <mail@tim-adler.de>
2023-01-10 21:56:19 +0100

    added solution

---
+++ block02-http/03-example-express-solution/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const app = express();
+
+const port = 3000;
+
+app.get("/", function(req, res) {
+    const name = "Class";
+
+    // console.log(req.query);
+    // let name = req.query.name;
+    // if (!name) {
+    //    name = "nobody in particular..." ;
+    // }
+    
+    res.send(`<h1>Hello ${name}</h1>`);
+
+});
+
+app.listen(port, function() {
+    console.log(`Listening on ${port}...`);
+});
