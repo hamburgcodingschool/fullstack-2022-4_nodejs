0f3f6c8f66be468b9a13e3a9db49d0a4adf86078
Tim Adler <mail@tim-adler.de>
2023-01-10 21:29:52 +0100

    added solution

---
+++ block02-http/01-example-simple-http-server-solution/index.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+//create a server
+
+http.createServer(function (req, res) {
+  res.writeHead(404, {'Content-Type': 'text/html'});
+  res.write('<html><head></head><body><button>I DO NOTHING</button></bod></html>'); //write a response to the client
+  res.end(); //end the response
+}).listen(8080); //the server object listens on port 8080
