6f6d47321fb00d7698b040574a0b5f1776c19393
Tim Adler <mail@tim-adler.de>
2023-01-17 22:30:27 +0100

    added example

---
+++ block05-webapp/01-example-web-mysql/index.js
@@ -0,0 +1,31 @@
+const _ = require("lodash");
+const express = require("express");
+const sass = require('node-sass-middleware')
+const path = require('path');
+
+
+const app = express();
+const port = 3000;
+
+app.set("view engine", "pug");
+app.use(
+  sass({
+    src: path.join(__dirname, 'sass'),
+    dest: path.join(__dirname, 'public'),
+    debug: true
+  })
+);
+app.use(express.static("public"))
+
+app.get("/", function(req, res) {
+  res.render("home");
+});
+
+app.get("*", function(req, res) {
+  res.status(404);
+  res.send("This page does not exist... It's probably your fault...");
+});
+
+app.listen(port, function() {
+  console.log(`Listening on ${port}...`);
+});
